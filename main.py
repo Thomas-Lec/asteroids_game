6178ddbfda7952a7e86e10cbc09c384eaf88f397
Thomas-Lec <tlechner246@gmail.com>
2025-06-17 19:22:56 +0200

    add clock + limit FPS

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ from constants import *  # import all constants
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return 
+                return
 
         screen.fill("black")
-
         pygame.display.flip()
+
+        #limit theh framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
