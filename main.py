655609ff3be36f13bd4bb06f057e35624a2f77c3
Thomas-Lec <tlechner246@gmail.com>
2025-07-27 19:21:39 +0200

    f/add colision

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import pygame
 
 from asteroid import Asteroid
@@ -30,6 +32,11 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
